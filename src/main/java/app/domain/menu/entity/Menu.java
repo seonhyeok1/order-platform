f5de6719ac5f3f9bcc238e8bb501399ac82491f3
bonun00 <lifesciences00@naver.com>
2025-07-25 16:14:34 +0900

    엔티티 수정

--- src/main/java/app/domain/menu/entity/Menu.java
+++ src/main/java/app/domain/menu/entity/Menu.java
@@ -1,11 +1,22 @@
 package app.domain.menu.entity;
 
+import java.util.UUID;
+
 import app.domain.store.entity.Store;
 import app.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.UUID;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "p_menu")
@@ -15,23 +26,23 @@ import java.util.UUID;
 @Builder
 public class Menu extends BaseEntity {
 
-    @Id
-    @GeneratedValue
-    private UUID menuId;
+	@Id
+	@GeneratedValue
+	private UUID menuId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "store_id", nullable = false)
-    private Store store;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "store_id", nullable = false)
+	private Store store;
 
-    @Column(nullable = false, length = 100)
-    private String name;
+	@Column(nullable = false, length = 100)
+	private String name;
 
-    @Column
-    private String desc;
+	@Column
+	private String description;
 
-    @Column(nullable = false)
-    private int price;
+	@Column(nullable = false)
+	private int price;
 
-    @Column(nullable = false)
-    private boolean isHidden = false;
+	@Column(nullable = false)
+	private boolean isHidden = false;
 }
--- src/main/java/app/domain/store/entity/Store.java
+++ src/main/java/app/domain/store/entity/Store.java
@@ -1,13 +1,24 @@
 package app.domain.store.entity;
 
+import java.util.UUID;
+
 import app.domain.menu.entity.Category;
 import app.domain.user.entity.User;
 import app.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.UUID;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "p_store")
@@ -17,37 +28,37 @@ import java.util.UUID;
 @Builder
 public class Store extends BaseEntity {
 
-    @Id
-    @GeneratedValue
-    private UUID storeId;
+	@Id
+	@GeneratedValue
+	private UUID storeId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "region_id", nullable = false)
-    private Region region;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "region_id", nullable = false)
+	private Region region;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="category_id", nullable = false)
-    private Category categoryId;
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_id", nullable = false)
+	private Category category;
 
-    @Column(nullable = false, length = 100)
-    private String storeName;
+	@Column(nullable = false, length = 100)
+	private String storeName;
 
-    @Column()
-    private String desc;
+	@Column
+	private String description;
 
-    @Column(nullable = false)
-    private String address;
+	@Column(nullable = false)
+	private String address;
 
-    @Column(length = 20)
-    private String phoneNumber;
+	@Column(length = 20)
+	private String phoneNumber;
 
-    @Column(nullable = false)
-    private int minOrderAmount;
+	@Column(nullable = false)
+	private int minOrderAmount;
 
-    @Column(nullable = false, length = 20)
-    private String storeAcceptStatus;
+	@Column(nullable = false)
+	private boolean storeAcceptStatus = true;
 }
