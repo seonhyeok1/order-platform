898695ec292ca1c55130ea5c8cf25df487cbc1d2
ColdTbrew <shchoi8687@hotmail.com>
2025-07-26 00:27:02 +0900

    fix() : seperate-db-cred use config
    DB login에 관한 user, pw
    yml -> .env 로 환경변수로 관리
    config로 .env 가져오기로 수정
    dotenv 의존성삭제

--- src/main/java/app/OrderPlatformBackendApplication.java
+++ src/main/java/app/OrderPlatformBackendApplication.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 @EnableJpaAuditing
 public class OrderPlatformBackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(OrderPlatformBackendApplication.class, args);
 	}
 
