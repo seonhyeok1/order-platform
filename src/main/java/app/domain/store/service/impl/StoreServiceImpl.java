90f2ea42c5da887e1cc5ee85456c50bac3f45780
kimseonhyeok123 <221978403+seonhyeok1@users.noreply.github.com>
2025-07-28 16:11:14 +0900

    feat(store): src/main/java/app/domain/store/service src/main/java/app/domain/store/service/impl
    -StoreService : 가게 등록 기능 서비스 인터페이스
    -- request, response 반환
    
    -StoreServiceImpl : StoreService 인터페이스
    -- Region 조회, 가게 정보 저장
    -- 가게 등록 > StoreApproveResponse 반환
    -- 예외 처리 > IllegalArgumentException

---
+++ src/main/java/app/domain/store/service/impl/StoreServiceImpl.java
@@ -0,0 +1,46 @@
+package app.domain.store.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import app.domain.store.model.dto.request.StoreApproveRequest;
+import app.domain.store.model.dto.response.StoreApproveResponse;
+import app.domain.store.model.entity.Region;
+import app.domain.store.model.entity.RegionRepository;
+import app.domain.store.model.entity.Store;
+import app.domain.store.model.entity.StoreRepository;
+import app.domain.store.model.type.StoreAcceptStatus;
+import app.domain.store.service.StoreService;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class StoreServiceImpl implements StoreService {
+
+	private final StoreRepository storeRepository;
+	private final RegionRepository regionRepository;
+
+	@Override
+	public StoreApproveResponse createStore(StoreApproveRequest request) {
+		Region region = regionRepository.findById(request.regionId())
+			.orElseThrow(() -> new IllegalArgumentException("해당 region이 존재하지 않습니다."));
+
+		Store store = Store.builder()
+			.storeName(request.storeName())
+			.region(region)
+			.address(request.address())
+			.description(request.desc())
+			.phoneNumber(request.phoneNumber())
+			.minOrderAmount(request.minOrderAmount().intValue())
+			.storeAcceptStatus(StoreAcceptStatus.PENDING)
+			.build();
+
+		Store savedStore = storeRepository.save(store);
+
+		return new StoreApproveResponse(
+			savedStore.getStoreId(),
+			savedStore.getStoreAcceptStatus().name()
+		);
+	}
+}
