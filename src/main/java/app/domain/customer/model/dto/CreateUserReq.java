3b2fdb83010fa56bedead984089206dc1f48def2
noeyah2 <hy0708@cau.ac.kr>
2025-07-29 19:31:48 +0900

    feat(customer): customer/model/dto/createUserReq 에 userRole을 추가해 정해진 값을 받도록 validUserRole 추가
    사용자 권한에 따라 저장되는 값과 해당하는 권한이 다르기 때문에 추가

--- src/main/java/app/domain/customer/model/dto/CreateUserReq.java
+++ src/main/java/app/domain/customer/model/dto/CreateUserReq.java
@@ -1,8 +1,11 @@
-package app.domain.user.model.dto;
+package app.domain.customer.model.dto;
 
+import app.domain.customer.model.entity.enums.UserRole;
+import app.global.validation.annotation.ValidUserRole;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -12,33 +15,38 @@ import lombok.Setter;
 @Setter
 public class CreateUserReq {
 
-	@NotBlank(message = "사용자 아이디는 필수입니다.")
+	@NotBlank
 	@Size(min = 4, max = 20, message = "사용자 아이디는 4자 이상 20자 이하로 입력해주세요.")
 	@Schema(description = "사용자 아이디", example = "testuser1")
 	private String username;
 
-	@NotBlank(message = "비밀번호는 필수입니다.")
-	@Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
+	@NotBlank
+	@Size(min = 8, max = 20)
 	@Schema(description = "비밀번호", example = "password123!")
 	private String password;
 
-	@NotBlank(message = "이메일은 필수입니다.")
-	@Email(message = "유효한 이메일 형식이 아닙니다.")
+	@NotBlank
+	@Email
 	@Schema(description = "이메일", example = "test@example.com")
 	private String email;
 
-	@NotBlank(message = "닉네임은 필수입니다.")
-	@Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하로 입력해주세요.")
+	@NotBlank
+	@Size(min = 2, max = 10)
 	@Schema(description = "닉네임", example = "테스트유저")
 	private String nickname;
 
-	@NotBlank(message = "실명은 필수입니다.")
-	@Size(min = 2, max = 50, message = "실명은 2자 이상 50자 이하로 입력해주세요.")
+	@NotBlank
+	@Size(min = 2, max = 50)
 	@Schema(description = "실명", example = "김구름")
 	private String realName;
 
-	@NotBlank(message = "전화번호는 필수입니다.")
-	@Pattern(regexp = "^\\d{10,11}$", message = "유효한 전화번호 형식이 아닙니다. ('-' 없이 숫자만 입력)")
+	@NotBlank
+	@Pattern(regexp = "^\\d{10,11}$")
 	@Schema(description = "전화번호('-' 없이 숫자만 입력)", example = "01012345678")
 	private String phoneNumber;
+
+	@NotNull
+	@ValidUserRole
+	@Schema(description = "사용자 권한", example = "CUSTOMER")
+	private UserRole userRole;
 }
