73ed9e0108fcdd1c6aad9b6e0338729c42bae522
huniieee <sehoon981014@gmail.com>
2025-07-30 14:03:39 +0900

    chore(all): jpaconfig 분리
    jpaconfig 분리
    
    ref#9

--- src/main/java/app/OrderPlatformBackendApplication.java
+++ src/main/java/app/OrderPlatformBackendApplication.java
@@ -2,10 +2,8 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class OrderPlatformBackendApplication {
 
 	public static void main(String[] args) {
