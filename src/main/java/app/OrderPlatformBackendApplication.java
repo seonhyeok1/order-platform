10c34323752ad20610eadca201e362b302521d2d
noeyah2 <hy0708@cau.ac.kr>
2025-07-30 13:09:40 +0900

    chore(auth): global/config에 JpaAuditingConfig 파일 추가 및 해당 부분 application에서 어노테이션 삭제
    test시 application의 EnableJpaAuditing과 동작방식이 겹쳐 오류나는 부분이 있어 따로 파일을 추가하고, OrderPlatformBackendApplication에서 어노테이션 삭제

--- src/main/java/app/OrderPlatformBackendApplication.java
+++ src/main/java/app/OrderPlatformBackendApplication.java
@@ -2,10 +2,9 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+// Test 관련 에러로 JpaAuditingConfig로 분리
 public class OrderPlatformBackendApplication {
 
 	public static void main(String[] args) {
