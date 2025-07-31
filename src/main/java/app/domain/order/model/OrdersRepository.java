ca0f1278802e107887c8550f78d6c115978f2978
bonun00 <lifesciences00@naver.com>
2025-07-31 14:16:35 +0900

    feat(order) order/model/orderRepository 배달 주소가 있는 주문 내역들을 조회
    오프라인 주문은 배달주소가 없으므로 배달 주소가 있는 주문 내역을 조회하도록 함

--- src/main/java/app/domain/order/model/OrdersRepository.java
+++ src/main/java/app/domain/order/model/OrdersRepository.java
@@ -2,9 +2,13 @@ package app.domain.order.model;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import app.domain.order.model.entity.Orders;
+import app.domain.user.model.entity.User;
 
 public interface OrdersRepository extends JpaRepository<Orders, UUID> {
+	Page<Orders> findAllByUserAndDeliveryAddressIsNotNull(User user, Pageable pageable);
 }
