907812def36f25bd73f7fc2d5b55180f27b4e7a3
noeyah2 <hy0708@cau.ac.kr>
2025-07-31 10:52:24 +0900

    feat(auth): auth/dto/response/LoginResponse, LoginRequest
    LoginRequest, LoginResponse

---
+++ src/main/java/app/domain/auth/model/dto/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package app.domain.auth.model.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+	@NotBlank
+	@Schema(description = "사용자 아이디", example = "testuser")
+	String username,
+
+	@NotBlank
+	@Schema(description = "사용자 비밀번호", example = "password123!")
+	String password
+) {
+}
