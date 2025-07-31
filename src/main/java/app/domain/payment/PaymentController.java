61b6fb9dd8e873bf45bdff22c2cd3199997eb7b7
huniieee <sehoon981014@gmail.com>
2025-08-01 00:08:14 +0900

    feat(payment) payment/controller 결제 controller 구현
    요청, 승인 controller 구현
    
    ref#36

---
+++ src/main/java/app/domain/payment/PaymentController.java
@@ -0,0 +1,53 @@
+package app.domain.payment;
+
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import app.domain.payment.model.dto.request.PaymentConfirmRequest;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class PaymentController {
+
+	private final PaymentService paymentService;
+
+	@GetMapping("/payment")
+	public String checkout(Model model) {
+		// Orders order = paymentService.getOrderById(orderId);
+		UUID test = UUID.randomUUID();
+		model.addAttribute("orderId", test);
+		model.addAttribute("totalPrice", 13000);
+
+		return "checkout";
+	}
+
+	@GetMapping("/success")
+	public String success(@RequestParam String paymentKey,
+		@RequestParam String orderId,
+		@RequestParam String amount,
+		Model model) {
+		model.addAttribute("paymentKey", paymentKey);
+		model.addAttribute("orderId", orderId);
+		model.addAttribute("amount", amount);
+		return "success";
+	}
+
+	@PostMapping("/confirm")
+	public ResponseEntity<String> confirm(@RequestBody PaymentConfirmRequest request) {
+		String result = paymentService.confirmPayment(request);
+		return ResponseEntity.ok(result);
+	}
+
+	@GetMapping("/fail")
+	public String fail() {
+		return "fail";
+	}
+}
