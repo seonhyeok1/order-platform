f73e65d5df1fb6fb84871ac59d2648a99df3c144
huniieee <sehoon981014@gmail.com>
2025-08-01 00:08:45 +0900

    feat(payment) payment 결제 Service 구현
    요청, 승인 service 구현
    
    ref#36

---
+++ src/main/java/app/domain/payment/PaymentService.java
@@ -0,0 +1,68 @@
+package app.domain.payment;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.UUID;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import app.domain.order.model.OrdersRepository;
+import app.domain.order.model.entity.Orders;
+import app.domain.payment.model.dto.request.PaymentConfirmRequest;
+import app.global.apiPayload.code.status.ErrorStatus;
+import app.global.apiPayload.exception.GeneralException;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+	private final OrdersRepository ordersRepository;
+
+	public Orders getOrderById(UUID orderId) {
+		return ordersRepository.findById(orderId)
+			.orElseThrow(() -> new GeneralException(ErrorStatus.ORDER_NOT_FOUND));
+	}
+
+	public String confirmPayment(PaymentConfirmRequest request) {
+		try {
+			String widgetSecretKey = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6";
+			Base64.Encoder encoder = Base64.getEncoder();
+			byte[] encodedBytes = encoder.encode((widgetSecretKey + ":").getBytes(StandardCharsets.UTF_8));
+			String authorizations = "Basic " + new String(encodedBytes);
+
+			JSONObject obj = new JSONObject();
+			obj.put("orderId", request.orderId());
+			obj.put("amount", request.amount());
+			obj.put("paymentKey", request.paymentKey());
+
+			URL url = new URL("https://api.tosspayments.com/v1/payments/confirm");
+			HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+			connection.setRequestProperty("Authorization", authorizations);
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setRequestMethod("POST");
+			connection.setDoOutput(true);
+
+			OutputStream outputStream = connection.getOutputStream();
+			outputStream.write(obj.toString().getBytes("UTF-8"));
+
+			int code = connection.getResponseCode();
+			boolean isSuccess = code == 200;
+
+			InputStream responseStream = isSuccess ? connection.getInputStream() : connection.getErrorStream();
+
+			if (isSuccess) {
+				return "결제 승인이 완료되었습니다. PaymentKey: " + request.paymentKey();
+			} else {
+				throw new GeneralException(ErrorStatus._INTERNAL_SERVER_ERROR);
+			}
+		} catch (Exception e) {
+			throw new GeneralException(ErrorStatus._INTERNAL_SERVER_ERROR);
+		}
+	}
+}
