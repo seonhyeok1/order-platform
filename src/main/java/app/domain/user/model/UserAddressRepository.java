0d73d383ff33e5d2e56d37f659527efbdf188727
yun1270 <modo000127@gmail.com>
2025-07-31 18:19:29 +0900

    chore(customer): user/model 엔티티, 레포지토리 수정 - UserAddressRepository 수정   - 메서드 추가 - entity/UserAddress 수정   - @Setter 추가

--- src/main/java/app/domain/user/model/UserAddressRepository.java
+++ src/main/java/app/domain/user/model/UserAddressRepository.java
@@ -2,9 +2,11 @@ package app.domain.user.model;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import app.domain.user.model.entity.UserAddress;
 
 public interface UserAddressRepository extends JpaRepository<UserAddress, UUID> {
 	List<UserAddress> findAllByUserUserId(Long UserId);
+	Optional<UserAddress> findByUser_UserIdAndIsDefaultTrue(Long userId);
 }
