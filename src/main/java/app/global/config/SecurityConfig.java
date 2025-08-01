34aec97c488f79733368acf897580de15b6950e3
huniieee <sehoon981014@gmail.com>
2025-08-01 11:11:15 +0900

    chore(payment) global/config 결제 관련 url 추가
    결제 요청,성공,실패,style.css 추가
    
    ref#36

--- src/main/java/app/global/config/SecurityConfig.java
+++ src/main/java/app/global/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
 					"/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources",
 					"/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**",
 					"/webjars/**", "/swagger-ui.html",
-					"/api/auth/signup", "/api/auth/login"
+					"/api/auth/signup", "/api/auth/login", "/payment/**", "/style.css"
 				).permitAll() // 위에 명시된 경로는 인증 없이 접근 허용
 				.anyRequest().authenticated() // 나머지 모든 경로는 인증 필요
 			);
