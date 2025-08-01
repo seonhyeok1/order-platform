975697d734d01388569dd8ecb283fd5fd4a52535
bonun00 <lifesciences00@naver.com>
2025-08-01 17:44:00 +0900

    feat(global) global/config/swaggerConfig 스웨거 설정 추가
    jwt 토큰 적용위해 설정 추가

---
+++ src/main/java/app/global/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package app.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("order API")
+				.description("order API 문서")
+				.version("v1.0.0"))
+			.components(new Components()
+				.addSecuritySchemes("bearer-jwt",
+					new SecurityScheme()
+						.type(SecurityScheme.Type.HTTP)
+						.scheme("bearer")
+						.bearerFormat("JWT")
+						.in(SecurityScheme.In.HEADER)
+						.name("Authorization")))
+			.addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
+	}
+}
