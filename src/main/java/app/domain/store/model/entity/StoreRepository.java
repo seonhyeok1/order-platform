9429bb6c08a76e1c3f4699199740b8f92674db92
bonun00 <lifesciences00@naver.com>
2025-08-01 13:53:11 +0900

    feat(store) store/StoreRepository 가게 id나 상태값으로 가게 조회
    가게 상태 값 및 가게 uuid로 조회

--- src/main/java/app/domain/store/model/entity/StoreRepository.java
+++ src/main/java/app/domain/store/model/entity/StoreRepository.java
@@ -3,8 +3,12 @@ package app.domain.store.model.entity;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import app.domain.store.model.enums.StoreAcceptStatus;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, UUID> {
@@ -12,5 +16,9 @@ public interface StoreRepository extends JpaRepository<Store, UUID> {
 	Optional<Store> findByUser_UserId(Long userId);
 
 	boolean existsByStoreNameAndRegion(String storeName, Region region);
+
+	Page<Store> findAllByStoreAcceptStatusAndDeletedAtIsNull(StoreAcceptStatus status,Pageable pageable);
+
+	Optional<Store> findByStoreIdAndDeletedAtIsNull(UUID storeId);
 
 }
