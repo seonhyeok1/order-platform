fd73b562994db9a314791fa45b0e03734fb97615
noeyah2 <hy0708@cau.ac.kr>
2025-08-02 17:01:39 +0900

    fix(user): user/model/dto/LoginResponse, LoginRequest dto record -> class
    변경사항
    1. NoArgsConstructor
    2. AllArgsConstructor : @Builder가 모든 필드를 사용하는 생성자를 찾을 수 있도록
    3. private : 캡슐화

--- src/main/java/app/domain/user/model/dto/response/LoginResponse.java
+++ src/main/java/app/domain/user/model/dto/response/LoginResponse.java
@@ -1,14 +1,22 @@
 package app.domain.user.model.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
-public record LoginResponse(
-	@Schema
-	String accessToken,
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
 
 	@Schema
-	String refreshToken
-) {
+	private String accessToken;
+
+	@Schema
+	private String refreshToken;
 }
