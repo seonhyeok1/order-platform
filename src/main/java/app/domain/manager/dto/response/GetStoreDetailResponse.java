d09a7eb1c79aab8c03a928967164dab36dbe5164
bonun00 <lifesciences00@naver.com>
2025-08-03 14:56:31 +0900

    fix(manager) manager/dto/response/GetStoreDetailResponse record->class dto
    가게 점주의 아이디 이메일등 정보추가

--- src/main/java/app/domain/manager/dto/response/GetStoreDetailResponse.java
+++ src/main/java/app/domain/manager/dto/response/GetStoreDetailResponse.java
@@ -3,21 +3,36 @@ package app.domain.manager.dto.response;
 import java.util.UUID;
 
 import app.domain.store.model.entity.Store;
+import app.domain.user.model.entity.User;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @Schema(description = "가게 상세 응답 DTO")
-public record GetStoreDetailResponse(
-	UUID storeId,
-	String storeName,
-	String description,
-	String address,
-	String phoneNumber,
-	long minOrderAmount,
-	String regionName,
-	String categoryName,
-	double averageRating
-) {
+@Getter
+@AllArgsConstructor
+public class GetStoreDetailResponse {
+
+	private UUID storeId;
+	private String storeName;
+	private String description;
+	private String address;
+	private String phoneNumber;
+	private long minOrderAmount;
+	private String regionName;
+	private String categoryName;
+	private double averageRating;
+
+	private Long ownerId;
+	private String ownerEmail;
+	private String ownerName;
+	private String ownerRealName;
+	private String ownerPhone;
+
+
 	public static GetStoreDetailResponse from(Store store, double avgRating) {
+		User user = store.getUser();
+
 		return new GetStoreDetailResponse(
 			store.getStoreId(),
 			store.getStoreName(),
@@ -27,7 +42,12 @@ public record GetStoreDetailResponse(
 			store.getMinOrderAmount(),
 			store.getRegion().getRegionName(),
 			store.getCategory().getCategoryName(),
-			avgRating
+			avgRating,
+			user.getUserId(),
+			user.getEmail(),
+			user.getUsername(),
+			user.getNickname(),
+			user.getPhoneNumber()
 		);
 	}
 }
