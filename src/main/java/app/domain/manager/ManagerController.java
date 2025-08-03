646a9d567dae44d07f4ea2d93bb145de1d8c7ebf
bonun00 <lifesciences00@naver.com>
2025-08-03 14:57:38 +0900

    fix(manager) manager/managerController swagger 설정수정
    @apiResponse삭제및 operation 추가

--- src/main/java/app/domain/manager/ManagerController.java
+++ src/main/java/app/domain/manager/ManagerController.java
@@ -6,28 +6,24 @@ import java.util.UUID;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import app.domain.manager.dto.response.GetCustomListResponse;
 import app.domain.manager.dto.response.GetCustomerDetailResponse;
+import app.domain.manager.dto.response.GetCustomerListResponse;
 import app.domain.manager.dto.response.GetStoreDetailResponse;
-import app.domain.manager.dto.response.GetStoreListResponse;
+import app.domain.customer.dto.response.GetStoreListResponse;
 import app.domain.order.model.dto.response.OrderDetailResponse;
 import app.domain.store.model.enums.StoreAcceptStatus;
 import app.global.apiPayload.ApiResponse;
 import app.global.apiPayload.PagedResponse;
-import app.global.apiPayload.code.status.ErrorStatus;
-import app.global.apiPayload.exception.GeneralException;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -40,48 +36,13 @@ public class ManagerController {
 	private final ManagerService managerService;
 
 	@GetMapping("/customer")
+	@SecurityRequirement(name = "bearer-key")
 	@Operation(
 		summary = "전체 사용자 목록 조회",
 		description = "가입한 사용자 목록을 페이지 별로 조회합니다. 생성일 또는 수정일 기준으로 정렬할 수 있습니다.")
-	@ApiResponses({
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(
-			responseCode = "200",
-			description = "사용자 목록 조회 성공",
-			content = @Content(
-				mediaType = "application/json",
-				schema = @Schema(implementation = ApiResponse.class),
-				examples = @ExampleObject(value = """
-					{
-					    "resultCode": "COMMON200",
-					    "message": "success",
-					    "result": {
-					        "content": [
-					            {
-					                "userId": 1,
-					                "email": "user1@example.com",
-					                "nickname": "사용자1",
-					                "createdAt": "2024-01-01T10:00:00"
-					            },
-					            {
-					                "userId": 2,
-					                "email": "user2@example.com",
-					                "nickname": "사용자2",
-					                "createdAt": "2024-01-02T11:00:00"
-					            },
-					            ...
-					        ],
-					        "page": 0,
-					        "size": 20,
-					        "totalElements": 100,
-					        "totalPages": 5
-					    },
-					}
-					""")
-			)
-		)
-	})
-	public ApiResponse<PagedResponse<GetCustomListResponse>> getAllCustomer(
-		@PageableDefault(size = 20, sort = "createdAt", direction = DESC) Pageable pageable
+	public ApiResponse<PagedResponse<GetCustomerListResponse>> getAllCustomer(
+		@PageableDefault(size = 20, sort = "createdAt", direction = DESC) Pageable pageable,
+		@AuthenticationPrincipal UserDetails principal
 	) {
 		return ApiResponse.onSuccess(managerService.getAllCustomer(pageable));
 	}
@@ -91,37 +52,6 @@ public class ManagerController {
 		summary = "선택한 유저 정보 조회",
 		description = "선택한 유저의 자세한 정보와 등록한 주소를 확인 합니다."
 	)
-	@ApiResponses({
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(
-			responseCode = "200",
-			description = "사용자 상세 정보 조회 성공",
-			content = @Content(
-				mediaType = "application/json",
-				schema = @Schema(implementation = ApiResponse.class),
-				examples = @ExampleObject(value = """
-					           {
-					               "resultCode": COMMON200,
-					               "message": success,
-					               "result": {
-					                   "userId": 1,
-					                   "email": "user1@example.com",
-					                   "nickname": "사용자1",
-					                   "createdAt": "2024-01-01T10:00:00",
-					                   "addresses": [
-					                       {
-					                           "addressId": 100,
-					                           "alias": "Home",
-					                           "address": "서울시 강남구 테헤란로 1",
-					                           "isDefault": true
-					                       },
-					                       ...
-					                   ]
-					               }
-					           }
-					""")
-			)
-		)
-	})
 	public ApiResponse<GetCustomerDetailResponse> getUsersDetailById(
 		@PathVariable("userId") Long userId
 	) {
@@ -133,41 +63,6 @@ public class ManagerController {
 		summary = "선택한 사용자 주문내역 조회",
 		description = "선택한 사용자의 주문 정보를 확인 합니다."
 	)
-	@ApiResponses({
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(
-			responseCode = "200",
-			description = "사용자 주문 내역 조회 성공",
-			content = @Content(
-				mediaType = "application/json",
-				examples = @ExampleObject(value = """
-					{
-					  "code": COMMON200,
-					  "message": "요청에 성공하였습니다.",
-					  "data": {
-					    "contents": [
-					      {
-					        "ordersId": "94d3d03a-6fd9-4c86-9cb4-188fc89d1b9e",
-					        "storeName": "한솥도시락 종로점",
-					        "totalPrice": 13500,
-					        "deliveryAddress": "서울특별시 종로구 종로 1",
-					        "paymentMethod": "CARD",
-					        "receiptMethod": "DELIVERY",
-					        "orderStatus": "DELIVERED",
-					        "isRefundable": false,
-					        "requestMessage": "문 앞에 놔주세요",
-					        "createdAt": "2025-07-28T14:35:00"
-					      }
-					    ],
-					    "totalPages": 1,
-					    "totalElements": 1,
-					    "page": 0,
-					    "size": 20
-					  }
-					}
-					""")
-			)
-		)
-	})
 	public ApiResponse<PagedResponse<OrderDetailResponse>> getCustomerOrderListById(
 		@PageableDefault(size = 20, sort = "createdAt", direction = DESC) Pageable pageable,
 		@PathVariable("userId") Long userId
@@ -179,100 +74,29 @@ public class ManagerController {
 	@Operation(
 		summary = "사용자 검색",
 		description = "키워드를 사용하여 가입한 사용자를 검색하고, 결과를 페이지 별로 조회합니다. 생성일 또는 수정일 기준으로 정렬할 수 있습니다.")
-	@ApiResponses({
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(
-			responseCode = "200",
-			description = "사용자 목록 조회 성공",
-			content = @Content(
-				mediaType = "application/json",
-				schema = @Schema(implementation = ApiResponse.class),
-				examples = @ExampleObject(value = """
-					{
-					    "resultCode": "COMMON200",
-					    "message": "success",
-					    "result": {
-					        "content": [
-					            {
-					                "userId": 1,
-					                "name":"홍길동",
-					                "email": "user1@example.com",
-					                "createdAt": "2024-01-01T10:00:00"
-					            },
-					            {
-					                "userId": 2,
-					                "name": "사용자2",
-					                "email": "user2@example.com",
-					                "createdAt": "2024-01-02T11:00:00"
-					            }
-					        ],
-					        "page": 0,
-					        "size": 20,
-					        "totalElements": 100,
-					        "totalPages": 5
-					    },
-					}
-					""")
-			)
-		)
-	})
-	public ApiResponse<PagedResponse<GetCustomListResponse>> searchCustomer(
+	public ApiResponse<PagedResponse<GetCustomerListResponse>> searchCustomer(
 		Pageable pageable,
 		@RequestParam String keyWord
 	) {
 		return ApiResponse.onSuccess(managerService.searchCustomer(keyWord, pageable));
 	}
 
-
-
 	@GetMapping("/store")
 	@Operation(
 		summary = "전체 가게 목록 조회",
 		description = "가게 목록을 페이지 별로 조회합니다. 생성일 또는 수정일 기준으로 정렬할 수 있습니다.")
-	@ApiResponses({
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(
-			responseCode = "200",
-			description = "가게 목록 조회 성공",
-			content = @Content(
-				mediaType = "application/json",
-				schema = @Schema(implementation = ApiResponse.class),
-				examples = @ExampleObject(value = """
-					{
-					    "resultCode": "COMMON200",
-					    "message": "success",
-					    "result": {
-					        "content": [
-					            {
-					                "userId": 1,
-					                "name":"홍길동"
-					                "email": "user1@example.com",
-					                "createdAt": "2024-01-01T10:00:00"
-					            },
-					            {
-					                "userId": 2,
-					                "name": "사용자2",
-					                "email": "user2@example.com",
-					                "createdAt": "2024-01-02T11:00:00"
-					            }
-					        ],
-					        "page": 0,
-					        "size": 20,
-					        "totalElements": 100,
-					        "totalPages": 5
-					    },
-					}
-					""")
-			)
-		)
-	})
 	public ApiResponse<PagedResponse<GetStoreListResponse>> getAllStore(
 		@PageableDefault(size = 20, sort = "createdAt", direction = DESC) Pageable pageable,
-		@RequestParam(defaultValue = "WAITING") StoreAcceptStatus status
+		@RequestParam(defaultValue = "APPORVE") StoreAcceptStatus status
 	) {
 		return ApiResponse.onSuccess(managerService.getAllStore( status,pageable));
 	}
 
 
 	@GetMapping("/store/{storeId}")
+	@Operation(
+		summary = "선택한 가게 상세 정보 조회",
+		description = "가게의 점주의 정보 및 가게의 정보를 조회 합니다")
 	public ApiResponse<GetStoreDetailResponse> getStoreById(
 		@PathVariable UUID storeId
 	){
@@ -280,6 +104,9 @@ public class ManagerController {
 	}
 
 	@PatchMapping("/store/{storeId}/accept")
+	@Operation(
+		summary = "선택한 가게 상태를 수정 합니다.",
+		description = "가게의 등록요청을 거절하거나 승인 합니다.")
 	public ApiResponse<String> approveStore(
 		@PathVariable UUID storeId,
 		@RequestParam StoreAcceptStatus status
@@ -287,8 +114,10 @@ public class ManagerController {
 		return ApiResponse.onSuccess(managerService.approveStore(storeId, status));
 	}
 
-
 	@GetMapping("/store/search")
+	@Operation(
+		summary = "가게를 검색합니다.",
+		description = "가게 제목에 따라서 검색 합니다.")
 	public ApiResponse<PagedResponse<GetStoreListResponse>> getAllStore(
 		@PageableDefault(size = 20, sort = "createdAt", direction = DESC) Pageable pageable,
 		@RequestParam(required = false) String keyword,
@@ -296,7 +125,5 @@ public class ManagerController {
 	) {
 		return ApiResponse.onSuccess(managerService.searchStore(status, keyword, pageable));
 	}
-
-
 
 }
