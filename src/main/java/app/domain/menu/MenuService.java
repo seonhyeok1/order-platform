5cbc6f94221efde907a518a8d688c478974ff955
bonun00 <lifesciences00@naver.com>
2025-08-03 14:46:16 +0900

    feat(menu) menu/MenuService 메뉴 조회 service
    스토어 아이디가 존재하지 않으면 예외처리

---
+++ src/main/java/app/domain/menu/MenuService.java
@@ -0,0 +1,37 @@
+package app.domain.menu;
+
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import app.domain.menu.model.MenuRepository;
+import app.domain.menu.model.dto.response.GetMenuListResponse;
+import app.domain.menu.model.entity.Menu;
+import app.domain.store.model.entity.StoreRepository;
+import app.global.apiPayload.PagedResponse;
+import app.global.apiPayload.code.status.ErrorStatus;
+import app.global.apiPayload.exception.GeneralException;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MenuService {
+
+	private final MenuRepository menuRepository;
+	private final StoreRepository storeRepository;
+
+	@Transactional(readOnly = true)
+	public PagedResponse<GetMenuListResponse> getMenusByStoreId(UUID storeId, Pageable pageable) {
+		boolean exists = storeRepository.existsByStoreIdAndDeletedAtIsNull(storeId);
+		if (!exists) {
+			throw new GeneralException(ErrorStatus.STORE_NOT_FOUND);
+		}
+		Page<Menu> menuPage = menuRepository.findByStoreStoreIdAndHiddenFalse(storeId, pageable);
+		Page<GetMenuListResponse> mapped = menuPage.map(GetMenuListResponse::from);
+
+		return PagedResponse.from(mapped);
+	}
+}
