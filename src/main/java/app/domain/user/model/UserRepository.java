52258c04e4ee7df846fbc483c809c36a4b7b5a84
noeyah2 <hy0708@cau.ac.kr>
2025-08-04 20:25:00 +0900

    feat(user): user/UserRepository-QueryDSL 기존 findFirstByUniqueFields 삭제
    1. 기존 findFirstByUniqueFields 삭제
    2. QueryDSL로 변경하고자 custom, customImpl 코드 추가

--- src/main/java/app/domain/user/model/UserRepository.java
+++ src/main/java/app/domain/user/model/UserRepository.java
@@ -5,29 +5,17 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import app.domain.user.model.entity.User;
 import app.domain.user.model.entity.enums.UserRole;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
 
 	Optional<User> findByUsername(String userName);
 
 	Optional<User> findByUserId(Long userId);
 
 	Page<User> findAllByUserRole(UserRole role, Pageable pageable);
-
-	// TODO : QUERYDSL로 수정 필요
-	@Query("SELECT u FROM User u WHERE u.username = :username OR u.email = :email OR u.nickname = :nickname OR u.phoneNumber = :phoneNumber")
-	Optional<User> findFirstByUniqueFields(
-		@Param("username") String username,
-		@Param("email") String email,
-		@Param("nickname") String nickname,
-		@Param("phoneNumber") String phoneNumber
-	);
-
 }
