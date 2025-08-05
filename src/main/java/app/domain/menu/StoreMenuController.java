5071ddcaffab9f48d82fa00b30228ca15d131957
huniieee <sehoon981014@gmail.com>
2025-08-05 21:37:45 +0900

    fix(all)

--- src/main/java/app/domain/menu/StoreMenuController.java
+++ src/main/java/app/domain/menu/StoreMenuController.java
@@ -27,7 +27,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/menu")
+@RequestMapping("/menu")
 @RequiredArgsConstructor
 public class StoreMenuController {
 
